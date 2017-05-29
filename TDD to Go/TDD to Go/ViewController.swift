14163006024c56891b87aee58c5a85852fedb470
Luay Younus <luayghazwan@yahoo.com>
2017-05-28 23:30:45 -0700

    Function to convert numbers to stringified numbers

--- TDD to Go/TDD to Go/ViewController.swift
+++ TDD to Go/TDD to Go/ViewController.swift
@@ -14,5 +14,10 @@ class ViewController: UIViewController {
         let vowels : [Character] = ["a","e","i","o","u","A","E","I","O","U"]
         return string.characters.reduce(0, { $0 + (vowels.contains($1) ? 1 : 0)})
     }
+    
+    func numbersToSpelledNumbers(numbers : [NSNumber]) -> [String] {
+        return numbers.map { NumberFormatter.localizedString(from: $0, number: .spellOut) }
+    }
+
 }
 
