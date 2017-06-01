55a7cefd6dec3bc2e9edbc3ec3e420e28e8c453c
Luay Younus <luayghazwan@yahoo.com>
2017-05-31 22:56:21 -0700

    Test to check if the Flatten array of string has optional Nil

--- TDD to Go/TDD to Go/ViewController.swift
+++ TDD to Go/TDD to Go/ViewController.swift
@@ -9,10 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        print(flattenAnArryWithoutOptionals(nestedArray:["A",nil,"B"]))
-    }
 
     func numberOfVowelsInString(string : String) -> Int {
         let vowels : [Character] = ["a","e","i","o","u","A","E","I","O","U"]
@@ -23,12 +19,13 @@ class ViewController: UIViewController {
         return numbers.map { NumberFormatter.localizedString(from: $0, number: .spellOut) }
     }
     
+    
     func flattenAnArryWithoutOptionals(nestedArray : [String?]) -> [String]{
         return nestedArray.flatMap{ $0 }
     }
     
     func sumArray(nums:[Int]) -> Int? {
-        let sum = nums.filter{$0 < 3}.reduce(0, {$0 + $1})
+        let sum = nums.filter{ $0 < 3 }.reduce(0, { $0 + $1 })
         if sum == 0 {
             return nil
         }
--- TDD to Go/TDD to GoTests/TDD_to_GoTests.swift
+++ TDD to Go/TDD to GoTests/TDD_to_GoTests.swift
@@ -13,30 +13,27 @@ class TDD_to_GoTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
+    //Number of Vowels in String using Reduce
     func testNumberOfVowelsInString_ShouldReturnNumberOfVowels() {
         
         let viewController = ViewController()
-        
         let name = "Luay"
-        
         let numberOfVowels = viewController.numberOfVowelsInString(string : name)
         
         XCTAssertEqual(numberOfVowels, 2, "should find 2 vowels in \(name)",file: "TDD_to_GoTests.swift", line: 24)
         
     }
     
+    //Convert Numbers into Spelled Numbers
     func testNumbersToSpelledNumbers() {
         
         let viewController = ViewController()
-        
         let arrayOfNumbers = [11,22,33,44]
         
         let stringifiedNumbers = viewController.numbersToSpelledNumbers(numbers: arrayOfNumbers as [NSNumber])
@@ -47,20 +44,37 @@ class TDD_to_GoTests: XCTestCase {
         
     }
     
+    //Flatten an Array without optional 'nil' using FlatMap
+    func testFlattenAnArryWithoutOptionals(){
+        
+        let viewController = ViewController()
+        var arrayWithoutNil:[String]? = viewController.flattenAnArryWithoutOptionals(nestedArray: ["Horse",nil,"Cat","Dog",nil,nil,"Parrot"])
+        
+        if (arrayWithoutNil?.count)! > 5 {
+            arrayWithoutNil = nil
+        }
+
+        XCTAssertNotNil(arrayWithoutNil,"The function is not excluding nils from the array")
+    }
+    
+    //Sum of Array using Filter and Reduce
     func testSumArray() {
+        
         let viewController = ViewController()
         let someArrayOfnums = [5,7,9,3,5,32,3,56,74]
-        
         let result = viewController.sumArray(nums: someArrayOfnums)
+        
         XCTAssertTrue(result == nil,"result should return nil back")
     }
     
     func testZipStudentsInfo_ShouldReturnArrayOfStudentsNamesAndGrades(){
+        
         let viewController = ViewController()
-    
         let result = viewController.zipStudentsInfo(names: ["Luay","Brandon"], grades: [88,89])
         for student in result {
             XCTAssertTrue(student is Dictionary<String, Any>, "Type returned is not dictionary of type [String:Any], check the function")
         }
     }
+    
+    //TDD
 }
